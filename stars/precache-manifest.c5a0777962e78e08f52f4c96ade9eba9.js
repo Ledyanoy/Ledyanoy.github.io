8560719b6220b2abea3771d84c5fd376a69544fa
korenev <ledaynoy44@hmail.com>
2020-09-09 22:52:17 +0400

    add stars

---
+++ stars/precache-manifest.c5a0777962e78e08f52f4c96ade9eba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fbf48285d223e064bc87aafe4c5147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b13d31fc0ee5a1b01d69",
+    "url": "/static/css/main.113ad0c9.chunk.css"
+  },
+  {
+    "revision": "7b3b6c6741c756c65a65",
+    "url": "/static/js/2.3d601272.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3d601272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13d31fc0ee5a1b01d69",
+    "url": "/static/js/main.739c2533.chunk.js"
+  },
+  {
+    "revision": "2b666024c89226609676",
+    "url": "/static/js/runtime-main.285bcf4f.js"
+  }
+]);
